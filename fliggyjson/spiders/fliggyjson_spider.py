fe94b2c4e14d3fffe6206467ded7fa406610d828
n490808114 <=>
2018-12-06 17:28:45 +0800

    0.12
    目前暂时跳过selenium,先复制浏览器cookies,转为字典，然后去爬
    抓取内容还要调整，存储估计也要调整
    需要完成的部分：
    1，分布式
    2，selenium自动获取，无头浏览器获取，不前台展示
    3，scrapy-splash还要继续，不过准备在fliggy羡慕中完成，那是另一个问题，lua脚本怎么才能继续加载数据，splash直接抓抓不到航班部分

--- fliggyjson/spiders/fliggyjson_spider.py
+++ fliggyjson/spiders/fliggyjson_spider.py
@@ -17,11 +17,11 @@ class Fliggyjsonspider(scrapy.Spider):
     allow_url = ['fliggy.com']
 
     # 接收起飞城市，抵达城市，开始日期，天数
-    depCityCode = input("depCityCode?")
-    arrCityCode = input("arrCityCode?")
+    depCityCode = input("depCityCode?\n")
+    arrCityCode = input("arrCityCode?\n")
     start_date = datetime.datetime.strptime(
-        input("please input scrapy start date"), '%Y-%m-%d')
-    times = int(input("how many days you want search?"))
+        input("please input scrapy start date\n"), '%Y-%m-%d')
+    times = int(input("how many days you want search?\n"))
 
     # 替换DEFAULT_REQUEST_HEADERS
     header = {
@@ -60,15 +60,18 @@ class Fliggyjsonspider(scrapy.Spider):
     print(start_urls)
 
     # 在url里面拿出来一个网址做登录测试
-
-    
-    test_url = start_urls.pop(0)
-    response_test = Request(test_url, headers=header)
+    cookies_list = str(input('Please input Cookies from internet!!!\n'))
+    cookies = {}
+    for line in cookies_list.split(';'):
+	    key,value=line.split('=',1)
+	    cookies[key] = value
+    #test_url = start_urls.pop(0)
+    #response_test = Request(test_url, headers=header)
     # 如果得到的响应网址是login网址，那么就登录登录到网站上，获取cookie
     # 并用cookie再次获取响应，再次测试
     # 同时把cookie传给start_request()函数
 
-    cookies = getCookies()
+    #cookies = getCookies()
     #cookies = "orderBy=undefined; __guid=74766352.1451806701806439200.1532330561021.38; _umdata=6DF8A7CF5CA7BAC2756318B76F0C18CBF3B143C2191B477D9ADC3C1AAC61DF48AC423B94F08E3504CD43AD3E795C914CAF29FA256100E0D3C340701FB36F69D7; hng=CN%7Czh-CN%7CCNY%7C156; lid=%E5%9B%9B%E5%B7%9D%E9%93%81%E8%88%AA%E5%95%86%E6%97%85; cna=NHPUExvnDkACAavd8fAcPWmV; t=2a22edebcf647cd89d97eac8d717026b; tracknick=%5Cu56DB%5Cu5DDD%5Cu94C1%5Cu822A%5Cu5546%5Cu65C5; _tb_token_=w9P8VO7Ds79kkdK9MqT8; cookie2=1d2c40bed781d6f3280bfbe0d3d7f787; dnk=%5Cu56DB%5Cu5DDD%5Cu94C1%5Cu822A%5Cu5546%5Cu65C5; sk=sai=UUjZelvO6U7stsCVwbFftg%3D%3D&n=qGxD%2FJkjMY5nzndI&sae=UUtO%2FV%2BJNKq8f6vDFdxC3h4%3D&agf=AHt%2BE4yWJ9vdW%2B3PG2H51AyjIfm77%2B3T1PCl1UVBPKI%3D&ss=Tmc%3D&fg=Vvj6Fvb8IA%3D%3D&agt=Ug%3D%3D&i=Vv7BmA%3D%3D&fn=qGxD%2FJkjMY4XrIJyKN%2BC20ZS6lobLAigB2M%3D&ln=qGxD%2FJkjMY5nzndI; tal=Vw%3D%3D; __guid=66414014.4475733110818210000.1540536894689.8647; uc1=cookie16=VFC%2FuZ9az08KUQ56dCrZDlbNdA%3D%3D&tmb=1&cookie21=UIHiLt3xSard&cookie15=UIHiLt3xD8xYTw%3D%3D&existShop=true&pas=0&cookie14=UoTYNcxtv7UJUw%3D%3D&tag=8&lng=zh_CN; csg=4b4b389a; ck=uid=UNJZJXuidSDLjQ%3D%3D; sty=2; enc=ac3moLzGNB8gKq7sZNJJxiNcxzhWuMhMt7wAdJnB8JVU1SQaW5jcA8A38Qi18Nq6kgsTjJ7DWv3PH%2BWpmN6N9Q%3D%3D; isg=BPHxpR4oCYSVd6LZxoE_UaEjAH0nznBToP23Z9MGz7jX-hFMGi8uID9bGM45Mv2I; monitor_count=50"
         
 
@@ -91,9 +94,11 @@ class Fliggyjsonspider(scrapy.Spider):
             f.write(response.text)
 
         # 删除json1234()保留括号中间的字典字符串，然后把字符串转为字典
-        site0 = re.search(r'json\d{,4}\((.*)\)', response.text).group()
+        site0 = re.search(r'{.*}', response.text).group()
         site1 = re.sub(r'{0:', r'{"0":', site0)
-        site = eval(site1)
+        site2 = re.sub(r'true',r'"true"',site1)
+        site3 = re.sub(r'false',r'"false"',site2)
+        site = eval(site3)
 
         # 打印一下格式,检验
         print(type(site))
