a30ee1413e8a2ad589ac0598dbb09e87b2a2f95d
n490808114 <=>
2018-12-14 18:02:34 +0800

    add

--- fliggyjson/test/for_test.py
+++ fliggyjson/test/for_test.py
@@ -1,6 +1,9 @@
 g = [1,2,3,4]
+print(id(g))
 def add(a,b):
     return a + b
 for n in[2,10]:
-	g = (add(n,i) for i in g)
+    print(n,list(g),id(g),'0001')
+    g = (add(n,i) for i in g)
+    print(n,list(g),id(g),'0002')
 print(list(g))
