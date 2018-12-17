b552539e12d2ef3728b2962087b4090d5fd8ded1
n490808114 <=>
2018-12-17 17:56:02 +0800

    change

--- fliggyjson/spiders/fliggyjson_spider.py
+++ fliggyjson/spiders/fliggyjson_spider.py
@@ -1,99 +1,49 @@
 import scrapy
-import time  # time.sleep()
 import re  # re.search()
-import datetime  # 日期遍历
-import codecs  # 存储response
-import sys  # 重大错误退出
 import urllib.parse  # unicode strings to chinese
-import random  # ksTS数字随机产生
 from ..items import FliggyjsonItem
 from scrapy import Request
 from scrapy import Selector
-import requests
+from .start_urls import get_start_urls
 # from .login import getCookies #暂时没有使用自动获取cookies
-
 
 class Fliggyjsonspider(scrapy.Spider):
     name = 'fliggyjson'
     allow_url = ['fliggy.com']
 
-    header = {
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.00',
-    }
 
-    # 接收起飞城市，抵达城市，开始日期，天数
+
     depCityCode = input("起飞城市？\n")
     arrCityCode = input("到达城市？\n")
     airlineCode = input("航空公司代码？\n（如果不指定航空公司，将搜索所有的直飞航班）\n")
     start_date = input("开始查询的日期？\n（如果不指定日期，将从今天查起）\n")
-    try:
-        if start_date == '':
-            start_date = datetime.datetime.now()
-        else:
-            start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
-    except:
-        print("错误的日期，日期格式YYYY-MM-DD")
+    search_for_each_week = input("请输入航班周期:\n(周一请输入1，周二请输入2.......周日请输入7.\n如果不限制航班周期，什么都不要输入)")
     times = int(input("查询多少天？\n"))
 
-    # 声明start_urls
-    start_urls = []
-    start_url_head = "https://sijipiao.fliggy.com/ie/flight_search_result_poller.do?"
-    for i in range(times):
+    start_urls = get_start_urls(depCityCode,arrCityCode,start_date,search_for_each_week,times)
 
-        # 处理网址ksTS和callback部分
-        tt0 = str(time.time()*1000)
-        a = tt0.split(".")[0]
-        tt1 = random.randint(0, 9999)
-        b = str(tt1)
-        c = str(tt1 + 1)
-
-        # 处理网址searchJourney部分
-        date = (start_date + datetime.timedelta(days=i)).strftime('%Y-%m-%d')
-        searchJourney = [{
-            "arrCityCode": arrCityCode,
-            "depCityCode": depCityCode,
-            "depDate": date,
-            "selectedFlights": []
-        }]
-
-        data = {
-            '_ksTS': f"{a}_{b}",
-            'callback': f"jsonp{c}",
-            'supportMultiTrip': True,
-            'searchBy': 1281,
-            'childPassengerNum': 0,
-            'infantPassengerNum': 0,
-            'searchJourney': str(searchJourney),
-            'tripType': 0,
-            'searchCabinType': 0,
-            'agentId': -1,
-            'controller': 1,
-            'searchMode': 0,
-            'b2g': 0,
-            'formNo': -1,
-            'cardId': None,
-            'needMemberPrice': None,
-        }
-        start_url = start_url_head + urllib.parse.urlencode(data)
-        start_urls.append(start_url)
-
-    #cookies_list = str(input('Please input Cookies from internet!!!\n'))
-    #cookies = {}
-    #for line in cookies_list.split(';'):
-    #    key, value = line.split('=', 1)
-    #    cookies[key] = value
+    cookies_list = str(input('Please input Cookies from internet!!!\n'))
+    cookies = {}
+    for line in cookies_list.split(';'):
+        key, value = line.split('=', 1)
+        cookies[key] = value
 
     def start_requests(self):
         for url in self.start_urls:
-            befor_depdate = re.findall(r'\d{4}-\d{2}-\d{2}',url)[0]
-            befor_url = f"https://sijipiao.fliggy.com/ie/flight_search_result.htm?searchBy=1280&tripType=0&depCity={self.depCityCode}&arrCity={self.arrCityCode}&depDate={befor_depdate}&arrDate="
-            r = requests.get(befor_url,headers=self.header)
-            print(r.status_code)
+            #befor_depdate = re.findall(r'\d{4}-\d{2}-\d{2}',url)[0]
+            #befor_url = f"https://sijipiao.fliggy.com/ie/flight_search_result.htm?searchBy=1280&tripType=0&depCity={self.depCityCode}&arrCity={self.arrCityCode}&depDate={befor_depdate}&arrDate="
+            #r = requests.get(befor_url,headers=self.header)
+            #print(r.status_code)
+            header = {
+                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+                'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.00',
+                'referer':re.sub(r'http(.*)searchby','https://sijipiao.fliggy.com/ie/flight_search_result.htm?searchBy',url)
+            }
             yield Request(url,
-                          cookies=r.cookies,
-                          callback=self.parse,
+                        headers=header,
+                        cookies=self.cookies,
+                        callback=self.parse,
                           )
 
     def parse(self, response):
@@ -123,8 +73,9 @@ class Fliggyjsonspider(scrapy.Spider):
                 item["depTimeStr"] = site["data"]["flightItems"][i]["flightInfo"][0]["depTimeStr"]
                 item["arrTimeStr"] = site["data"]["flightItems"][i]["flightInfo"][0]["arrTimeStr"]
                 item["price"] = site["data"]["flightItems"][i]["cardTotalPrice"]/100
-                print(item)
+                #print(item)
                 items.append(item)
-        if item == {}:
-            print(f'{response_date}没有你要找的航班')
+                item = {}
+        #if item == {}:
+        #    print(f'{response_date}没有你要找的航班')
         return items
---
+++ fliggyjson/spiders/url.py
@@ -0,0 +1,64 @@
+import datetime  # 日期遍历
+import random  # ksTS数字随机产生
+import time
+import urllib.parse
+
+
+def get_start_urls(*args):
+
+    # 接收起飞城市，抵达城市，开始日期，天数
+
+    depCityCode,arrCityCode,start_date,search_for_each_week,times =args
+    try:
+        if start_date == '':
+            start_date = datetime.datetime.now()
+        else:
+            start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    except:
+        print("错误的日期，日期格式YYYY-MM-DD")
+    
+
+    # 声明start_urls
+    start_urls = []
+    start_url_head = "https://sijipiao.fliggy.com/ie/flight_search_result_poller.do?"
+    for i in range(times):
+
+        # 处理网址ksTS和callback部分
+        tt0 = str(time.time()*1000)
+        a = tt0.split(".")[0]
+        tt1 = random.randint(0, 9999)
+        b = str(tt1)
+        c = str(tt1 + 1)
+
+        # 处理网址searchJourney部分
+        date = start_date + datetime.timedelta(days=i)
+        if (search_for_each_week != '') and (int(date.weekday+1) not in search_for_each_week):
+            continue
+        searchJourney = [{
+            "arrCityCode": arrCityCode,
+            "depCityCode": depCityCode,
+            "depDate": date.strftime('%Y-%m-%d'),
+            "selectedFlights": []
+        }]
+
+        data = {
+            '_ksTS': f"{a}_{b}",
+            'callback': f"jsonp{c}",
+            'supportMultiTrip': True,
+            'searchBy': 1281,
+            'childPassengerNum': 0,
+            'infantPassengerNum': 0,
+            'searchJourney': str(searchJourney),
+            'tripType': 0,
+            'searchCabinType': 0,
+            'agentId': -1,
+            'controller': 1,
+            'searchMode': 0,
+            'b2g': 0,
+            'formNo': -1,
+            'cardId': None,
+            'needMemberPrice': None,
+        }
+        start_url = start_url_head + urllib.parse.urlencode(data)
+        start_urls.append(start_url)
+    return start_urls
